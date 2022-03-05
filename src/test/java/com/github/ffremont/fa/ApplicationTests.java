906d0bad5b03b233786ad74e0d9049cada7e15e3
Florent FREMONT <ff.fremont.florent@gmail.com>
2022-03-05 10:42:29 +0100

    first commit

---
+++ src/test/java/com/github/ffremont/fa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ffremont.fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
